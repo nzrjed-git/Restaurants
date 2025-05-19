13e233642bbf7093dca2e1b5428d0b08a94a8366
Nazar Sachuk <nazar.sachuk.dev@gmail.com>
2025-05-19 18:00:49 +0300

    Revert "feat: add property restaurantId"
    This reverts commit f4b46a0171bd410dd3c1af21a3d2e01ba4200b52.

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -6,7 +6,5 @@
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public decimal Price { get; set; }
-
-        public int RestaurantId { get; set; }
     }
 }

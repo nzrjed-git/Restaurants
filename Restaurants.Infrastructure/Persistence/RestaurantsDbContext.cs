26f11f442e789d9e9e3da156ee6ad3661a59cce6
Nazar Sachuk <nazar.sachuk.dev@gmail.com>
2025-05-19 17:46:47 +0300

    feat: init dbContext and installed ef core apckages

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence
+{
+    public class RestaurantsDbContext: DbContext
+    {
+        internal DbSet<Restaurant> Restaurants { get; set; }
+        internal DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.
+                UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=RestaurantsDb;Trusted_Connection=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Restaurant>()
+                .OwnsOne(r => r.Address);
+        }
+    }
+}

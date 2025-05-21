72aefb8d12c75c1610c6e03e55121208fc717c53
Nazar Sachuk <nazar.sachuk.dev@gmail.com>
2025-05-21 20:50:01 +0300

    feat: add logging to file and replaced configuration to appsetting.Development.json

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -14,9 +14,11 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Host.UseSerilog((context, configuration) =>
 {
     configuration
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-        .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}");
+        .ReadFrom.Configuration(context.Configuration);
+        //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        //.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+        //.WriteTo.File("Logs/Restaurant-API-.log", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+        //.WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}");
 });
 
 var app = builder.Build();
